12d60dee82a9d7c405373d2ea1b9055204d457e8
gogames <1805601022@qq.com>
2014-08-10 00:11:36 +0800

    debug session

--- auth_hall_server/session.go
+++ auth_hall_server/session.go
@@ -6,6 +6,8 @@ import "github.com/gogames/go_tetris/utils"
 var session = utils.NewSessionStore()
 
 func initSession() {
+	log.Info("successfully init session...")
 	session.Init(querySessions())
 	deleteSessions()
+	log.Info("all sessions: %v", session)
 }
