dffe9fc59018ca5f7f3e90fccdf95aeecaaa27e4
gogames <1805601022@qq.com>
2014-08-10 22:46:17 +0800

    get real ip, first remote address then http header

--- utils/ip.go
+++ utils/ip.go
@@ -3,8 +3,15 @@ package utils
 import "strings"
 
 func GetIp(ctx interface{}) string {
+	ip := strings.Split(getContext(ctx).Request.RemoteAddr, ":")[0]
+	if ip != "127.0.0.1" {
+		return ip
+	}
 	if realIp := getContext(ctx).Request.Header.Get("X-Real-Ip"); realIp != "" {
 		return realIp
 	}
-	return strings.Split(getContext(ctx).Request.RemoteAddr, ":")[0]
+	if realIp := getContext(ctx).Request.Header.Get("X-Forwaded-For"); realIp != "" {
+		return realIp
+	}
+	return ""
 }
