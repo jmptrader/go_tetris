fd183ac05c67a25b92921d0d2cb4d61b0c3bbebb
gogames <1805601022@qq.com>
2014-08-14 12:11:39 +0800

    update transparent

--- tetris/color.go
+++ tetris/color.go
@@ -20,46 +20,47 @@ func (c Color) String() string {
 }
 
 func (c Color) toTransparent() Color {
-	return Color(-int(c))
+	return Color_transparent
 }
 
 func (c Color) isStone() bool {
-	return int(c) == Color_stone
+	return c == Color_stone
 }
 
 func (c Color) isNothing() bool {
-	return int(c) == Color_nothing
+	return c == Color_nothing
 }
 
 func (c Color) isTransparent(oc Color) bool {
-	return int(c) == -int(oc)
+	return oc == Color_transparent
 }
 
 func (c Color) isBomb() bool {
-	return int(c) == Color_bomb
+	return c == Color_bomb
 }
 
 const (
 	maxColor = 7
 
-	Color_nothing = 0
-	Color_stone   = -99
-	Color_bomb    = -98
+	Color_nothing     = Color(0)
+	Color_stone       = Color(-99)
+	Color_bomb        = Color(-98)
+	Color_transparent = Color(-1)
 )
 
 // Colors
-var Colors = map[int]string{
-	Color_nothing: "nothing",
-	Color_stone:   "stone",
-	Color_bomb:    "bomb",
-
-	1:  "black",
-	2:  "red",
-	3:  "green",
-	4:  "blue",
-	5:  "yellow",
-	6:  "pink",
-	7:  "purple",
-	-7: "transparent-purple", // the negative value represents the transparent Color
-	-6: "transparent-pink",   // ...
-}
+// var Colors = map[int]string{
+// 	Color_nothing: "nothing",
+// 	Color_stone:   "stone",
+// 	Color_bomb:    "bomb",
+//
+// 	1:  "black",
+// 	2:  "red",
+// 	3:  "green",
+// 	4:  "blue",
+// 	5:  "yellow",
+// 	6:  "pink",
+// 	7:  "purple",
+// 	-7: "transparent-purple", // the negative value represents the transparent Color
+// 	-6: "transparent-pink",   // ...
+// }
--- tetris/zone.go
+++ tetris/zone.go
@@ -203,7 +203,7 @@ func (m mainZone) beingKO() bool {
 func (zone ZoneData) clear() ZoneData {
 	for h, l := range zone {
 		for w, _ := range l {
-			zone[h][w] = newColor(Color_nothing)
+			zone[h][w] = Color_nothing
 		}
 	}
 	return zone
