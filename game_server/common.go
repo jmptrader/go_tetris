30e32f8b64a7e41c5444d7405daed840953dbd96
gogames <1805601022@qq.com>
2014-08-17 12:34:30 +0800

    no json unmarshal

--- game_server/common.go
+++ game_server/common.go
@@ -1,28 +1,29 @@
 package main
 
-import (
-	"encoding/json"
-	"fmt"
-)
+import "fmt"
 
 // response from game server to client
 type responseData struct {
-	Description string      `json:"desc"`
-	Data        interface{} `json:"data"`
+	Desc string      `json:"desc"`
+	Data interface{} `json:"data"`
 }
 
 func newResponse(desc string, data interface{}) responseData {
-	return responseData{Description: desc, Data: data}
+	return responseData{Desc: desc, Data: data}
 }
 
 func (r responseData) String() string {
-	return fmt.Sprintf("\ndescription: %v, data: %v", r.Description, r.Data)
+	return fmt.Sprintf("\ndescription: %v, data: %v", r.Desc, r.Data)
 }
 
-func (r responseData) toJson() string {
-	b, err := json.Marshal(r)
-	if err != nil {
-		log.Debug("can not json marshal the data %v: %v", r, err)
-	}
-	return string(b)
+func (r responseData) toJson() interface{} {
+	return r
 }
+
+// func (r responseData) toJson() string {
+// 	b, err := json.Marshal(r)
+// 	if err != nil {
+// 		log.Debug("can not json marshal the data %v: %v", r, err)
+// 	}
+// 	return string(b)
+// }
